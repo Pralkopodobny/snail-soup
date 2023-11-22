67800b755bf175d404de7bb0da136deaf994c1d7
Pralkopodobny <43888534+Pralkopodobny@users.noreply.github.com>
2023-11-22 19:32:45 +0100

    add swagger

--- snailsoup-backend/src/features/expense/handlers.rs
+++ snailsoup-backend/src/features/expense/handlers.rs
@@ -1,6 +1,8 @@
-use crate::ExpenseService;
+use crate::{domain, ExpenseService};
+use serde::{Deserialize, Serialize};
 use std::convert::Infallible;
 use std::sync::Arc;
+use utoipa::ToSchema;
 use warp::hyper::StatusCode;
 
 pub async fn expense_by_id(
@@ -20,8 +22,42 @@ pub async fn expense_by_id(
     })))
 }
 
+#[utoipa::path(
+    get,
+    path = "/api/admin/expenses",
+    tag = "Expenses",
+    responses(
+        (status = 200, description = "list expenses successfully", body = [ExpenseResponse])
+    )
+)]
 pub async fn all_expenses(db: Arc<ExpenseService>) -> Result<impl warp::Reply, Infallible> {
-    let expenses = db.get_all().await;
+    let expenses : Vec<ExpenseResponse> = db.get_all().await.into_iter().map(|e| ExpenseResponse::from_expense(e)).collect();
 
-    Ok(warp::reply::html(expenses.len().to_string()))
+    Ok(warp::reply::json(&expenses))
+}
+
+#[derive(Serialize, Deserialize, ToSchema, Clone)]
+pub struct ExpenseResponse {
+    #[schema()]
+    pub id: uuid::Uuid,
+    #[schema()]
+    pub user_id: uuid::Uuid,
+    #[schema()]
+    pub description: Option<String>,
+    #[schema()]
+    pub expense_date: chrono::NaiveDate,
+    #[schema()]
+    pub cost: rust_decimal::Decimal,
+}
+
+impl ExpenseResponse {
+    pub fn from_expense(expense: domain::Expense) -> ExpenseResponse {
+        ExpenseResponse {
+            id: expense.id,
+            user_id: expense.user_id,
+            description: expense.description,
+            expense_date: expense.expense_date,
+            cost: expense.cost,
+        }
+    }
 }
--- snailsoup-backend/src/features/expense/mod.rs
+++ snailsoup-backend/src/features/expense/mod.rs
@@ -1,4 +1,6 @@
 mod api;
-mod handlers;
+pub mod handlers;
 
 pub use api::expense_filters;
+
+pub use handlers::all_expenses;

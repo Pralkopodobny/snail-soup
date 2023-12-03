7ce5f4e63e0f6b387d61e2478e3957c73fd97946
Pralkopodobny <43888534+Pralkopodobny@users.noreply.github.com>
2023-12-03 16:11:18 +0100

    make username unique

--- snailsoup-backend/migrations/20231023191316_init.up.sql
+++ snailsoup-backend/migrations/20231023191316_init.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS app_users (
     id UUID PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
     account_role VARCHAR(20) NOT NULL,
     CONSTRAINT role_check CHECK(account_role IN ('Admin', 'User'))

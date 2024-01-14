fce0bae258d5a25f168c4508aec9a086ecf8d509
Pralkopodobny <43888534+Pralkopodobny@users.noreply.github.com>
2024-01-14 21:27:43 +0100

    Add insert/delete methods to expense repository

--- snailsoup-backend/src/db/expense_repository.rs
+++ snailsoup-backend/src/db/expense_repository.rs
@@ -76,6 +76,32 @@ impl ExpenseRepository {
         Ok(tags)
     }
 
+    pub async fn insert_tag(&self, tag: Tag) -> Result<Uuid, sqlx::Error> {
+        let id = sqlx::query_scalar!(
+            r#"
+            INSERT INTO user_tags (id, user_id, name) VALUES ($1, $2, $3) RETURNING id
+            "#,
+            tag.id,
+            tag.user_id,
+            tag.name
+        ).fetch_one(&self.pool)
+        .await?;
+
+        Ok(id)
+    }
+
+    pub async fn delete_tag(&self, tag_id: Uuid) -> Result<Option<Uuid>, sqlx::Error> {
+        let id = sqlx::query_scalar!(
+            r#"
+            DELETE FROM user_tags WHERE id = $1 RETURNING id
+            "#,
+            tag_id
+        ).fetch_optional(&self.pool)
+        .await?;
+
+        Ok(id)
+    }
+
     pub async fn get_all_categories_by_user_id(
         &self,
         user_id: Uuid,
@@ -93,5 +119,47 @@ impl ExpenseRepository {
         .await?;
 
         Ok(categories)
+    }
+
+    pub async fn insert_category(&self, category: Category) -> Result<Uuid, sqlx::Error> {
+        let id = sqlx::query_scalar!(
+            r#"
+            INSERT INTO user_categories (id, user_id, name) VALUES ($1, $2, $3) RETURNING id
+            "#,
+            category.id,
+            category.user_id,
+            category.name
+        ).fetch_one(&self.pool)
+        .await?;
+
+        Ok(id)
+    }
+
+    pub async fn delete_category(&self, category_id: Uuid) -> Result<Option<Uuid>, sqlx::Error> {
+        let id = sqlx::query_scalar!(
+            r#"
+            DELETE FROM user_categories WHERE id = $1 RETURNING id
+            "#,
+            category_id
+        ).fetch_optional(&self.pool)
+        .await?;
+
+        Ok(id)
+    }
+
+    pub async fn delete_category_force(&self, category_id: Uuid) -> Result<Option<Uuid>, sqlx::Error> {
+        let mut transaction = self.pool.begin().await?;
+        sqlx::query!("UPDATE expenses SET category_id = NULL WHERE category_id = $1", category_id).execute(&mut *transaction).await?;
+        let id = sqlx::query_scalar!(
+            r#"
+            DELETE FROM user_categories WHERE id = $1 RETURNING id
+            "#,
+            category_id
+        ).fetch_optional(&mut *transaction)
+        .await?;
+
+        transaction.commit().await?;
+
+        Ok(id)
     }
 }

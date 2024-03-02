71184d2f20bd959c8ac1d0b64d2cb2c621ae9ace
Pralkopodobny <43888534+Pralkopodobny@users.noreply.github.com>
2024-03-02 15:54:49 +0100

    Add auto-migration and db creation at startup

--- snailsoup-backend/src/main.rs
+++ snailsoup-backend/src/main.rs
@@ -9,7 +9,7 @@ mod utils;
 use std::sync::Arc;
 
 use dotenv::dotenv;
-use sqlx::postgres::PgPoolOptions;
+use sqlx::{migrate::MigrateDatabase, postgres::PgPoolOptions, Pool, Postgres};
 
 use crate::{
     app_state::AppState,
@@ -17,22 +17,59 @@ use crate::{
     services::{auth::AuthService, expense::ExpenseService, user::UserService},
 };
 
+async fn connect_to_db() -> Result<Pool<Postgres>, String> {
+    let connection_string = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    if !sqlx::Postgres::database_exists(connection_string.as_str())
+        .await
+        .map_err(|_| "Error checking if db exists")?
+    {
+        sqlx::Postgres::create_database(connection_string.as_str())
+            .await
+            .map_err(|_| "Error creating db")?;
+    }
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(connection_string.as_str())
+        .await
+        .map_err(|_| "Cannot connect to database!")?;
+
+    let migrations = if std::env::var("RUST_ENV") == Ok("production".to_string()) {
+        // Productions migrations dir
+        std::env::current_exe()
+            .map_err(|e| e.to_string())?
+            .parent()
+            .ok_or("Unexpected Error when creating migrations path")?
+            .join("./migrations")
+    } else {
+        // Development migrations dir
+        let crate_dir = std::env::var("CARGO_MANIFEST_DIR").map_err(|e| e.to_string())?;
+        std::path::Path::new(&crate_dir).join("./migrations")
+    };
+
+    println!("Trying to do a migration based on {}", migrations.display());
+
+    sqlx::migrate::Migrator::new(migrations)
+        .await
+        .map_err(|e| e.to_string())?
+        .run(&pool)
+        .await
+        .map_err(|e| e.to_string())?;
+
+    Ok(pool)
+}
+
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
-    let connection_string = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
-    let pool = match PgPoolOptions::new()
-        .max_connections(5)
-        .connect(connection_string.as_str())
-        .await
-    {
-        Ok(p) => p,
-        Err(_) => panic!("Cannot connect to database!"),
-    };
-
     println!("Connected to a database");
+
+    let pool = match connect_to_db().await {
+        Ok(p) => p,
+        Err(e) => panic!("{}", e),
+    };
 
     let config = Config::init();
     println!(

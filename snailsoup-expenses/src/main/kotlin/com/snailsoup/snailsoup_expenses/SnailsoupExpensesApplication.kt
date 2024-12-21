ddb00044a5206496de366611b3c2eb6231d29aa7
Marcel <51127184+Greensie@users.noreply.github.com>
2024-12-21 13:29:41 +0100

    Add snailsoup-expenses

---
+++ snailsoup-expenses/src/main/kotlin/com/snailsoup/snailsoup_expenses/SnailsoupExpensesApplication.kt
@@ -0,0 +1,11 @@
+package com.snailsoup.snailsoup_expenses
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnailsoupExpensesApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnailsoupExpensesApplication>(*args)
+}
---
+++ snailsoup-expenses/src/test/kotlin/com/snailsoup/snailsoup_expenses/SnailsoupExpensesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.snailsoup.snailsoup_expenses
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnailsoupExpensesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
